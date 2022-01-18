3a3c9da4d10e8a1ed831ae243abf1130cf7fa2bd
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-01-18 12:32:00 +0000

    creating moodtracker emojis

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -17,6 +17,9 @@ function App() {
   );
 }
 
+
+
+
 export default App;
 
 
--- src/Components/Dayview/DayviewComp/Moodtracker/index.js
+++ src/Components/Dayview/DayviewComp/Moodtracker/index.js
@@ -2,7 +2,16 @@ import React from "react";
 
 export default function Moodtracker(){
     return(
+
     <div>
+        <span>
+            <h1>How Are you feeling Today?</h1>
+            <button1>👿</button1>
+            <button2>😥</button2>
+            <button3>😐</button3>
+            <button4>🙂</button4>
+            <button5>😁</button5>
+        </span>
       
      </div>
     )
