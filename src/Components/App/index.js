09f9152da604f167c4659739d69cc6eefaa1a702
callumhemming <callum.m.hemming@outlook.com>
2022-01-18 15:26:55 +0000

    Commented out for stability, commiting to netfiy

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "../Layout"
-import Homepage from "../Homepage"
-import Dayview from "../Dayview"
-import ToDoList from "../ToDoList"
+// import Layout from "../Layout"
+// import Homepage from "../Homepage"
+// import Dayview from "../Dayview"
+// import ToDoList from "../ToDoList"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {/* Layout will have an ul inside a nav tag with Links to ToDoList and Homepage */}
-          <route path="/" element={<Layout />} /> 
-          {/* Homepage will have h1 Hello bootcamper, h2 You're on week x, and the calander*/}
-          <route index element={<Homepage />}> 
-          {/* Day view will have Mood tracker component and Journal component */}
-            <route path="dayview" element={<Dayview />} />
-            {/* Make a to-do list */}
-            <route path="GlobalToDoList" element={<ToDoList />} />
-          </route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <h1>Hello world</h1>
+    </div> 
   );
 }
 
 export default App;
+
+
+      // {/* {/* <BrowserRouter>
+      //   <Routes>
+      //     {/* Layout will have an ul inside a nav tag with Links to ToDoList and Homepage */}
+      //     <route path="/" element={<Layout />} /> 
+      //     {/* Homepage will have h1 Hello bootcamper, h2 You're on week x, and the calander*/}
+      //     <route index element={<Homepage />}> 
+      //     {/* Day view will have Mood tracker component and Journal component */}
+      //       <route path="dayview" element={<Dayview />} />
+      //       {/* Make a to-do list */}
+      //       <route path="GlobalToDoList" element={<ToDoList />} />
+      //     </route>
+      //   </Routes>
+      // </BrowserRouter>
