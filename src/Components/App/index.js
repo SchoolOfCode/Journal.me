87a97b35739c5f289e76777e210d5152093512ca
Tan Matthews <tancodinggeneration@gmail.com>
2022-01-18 14:46:24 +0000

    rendered calender to page, also shows what date you have selected. Also have put calender in its component folder

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
+import React from "react";
+import MyCalender from "../Calender";
+/* import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import moment from "moment"; */
 
-function App() {
-  const [value, onChange] = useState(new Date());
+/* function App() {
+  const [date, setDate] = useState(new Date());
+
+  function changeDate(event) {
+    setDate(event);
+  }
 
   return (
     <div>
-      <h1>Hello</h1>
-      <Calendar onChange={onChange} value={value} />
+      <h1>Hello, Bootcamper</h1>
+      <Calendar onChange={changeDate} value={date} />
+      <p>You have selected: {moment(date).format("MMMM Do YYYY")}</p>
     </div>
+  );
+} */
+
+function App() {
+  return (
+    <>
+      <MyCalender />
+    </>
   );
 }
 
--- src/Components/Calender/index.js
+++ src/Components/Calender/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import moment from "moment";
 
-export default function ourCalender() {
+function MyCalender() {
+  const [date, setDate] = useState(new Date());
+
+  function changeDate(event) {
+    setDate(event);
+  }
+
   return (
-    <>
-      <Calendar />
-    </>
+    <div>
+      <Calendar onChange={changeDate} value={date} />
+      <p>You have selected: {moment(date).format("MMMM Do YYYY")}</p>
+    </div>
   );
 }
+
+export default MyCalender;
