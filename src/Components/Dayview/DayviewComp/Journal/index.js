061b1d74b8f08eb1d603fcab9abc3030a7d4a5e6
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-01-19 16:37:46 +0000

    styling the input box in journal

--- src/Components/Dayview/DayviewComp/Journal/index.js
+++ src/Components/Dayview/DayviewComp/Journal/index.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from "react";
 import { actions, reduceFormData } from "../libs/actions.js";
+import "./style.css";
 
 
 export default function Journal() {
@@ -16,23 +17,27 @@ export default function Journal() {
 
     <form>
       <h1> Our Daily Journal </h1>
-      <input
-        placeholder="Title"
-        onChange={e => {
-          formDataDispatch({ type: actions.TITLE, value: e.target.value });
-        }}
-      />
-      <input
-        placeholder="Body"
-        onChange={e => {
-          formDataDispatch({ type: actions.BODY, value: e.target.value });
-        }}
-      />
-      <button  id= "submit" onClick={e => { e.preventDefault();
-        alert("Working")
-        }}>
-        Submit
-      </button>
+
+      <container id="journal-input" >
+        <input id ="textbox-title"
+          placeholder="Title"
+          onChange={e => {
+            formDataDispatch({ type: actions.TITLE, value: e.target.value });
+          }}
+        />
+        <input id ="textbox-body"
+          placeholder="Body"
+          onChange={e => {
+            formDataDispatch({ type: actions.BODY, value: e.target.value });
+          }}
+        />
+        </container>
+        <button  id= "submit" onClick={e => { e.preventDefault();
+          alert("Working")
+          }}>
+          Submit
+        </button>
+
     </form>
   );
 }
