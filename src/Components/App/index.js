a6af8c65c9d248531bca25308381df839239fd54
callumhemming <callum.m.hemming@outlook.com>
2022-01-20 12:17:01 +0000

    fixing error

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect,useReducer } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "../Layout";
 import Homepage from "../Homepage";
 import Dayview from "../Dayview";
-import TodoList from "../TodoList";
+import TodoList from "./TodoList";
+
+
+function reduceUserData(state, actions){
+  return actions.newState
+}
+
 
 export default function App() {
-  const [userId, setUserId] = useState(0);
+  const [userId, setUserId] = useState(1);
+  // const [userData, setUserData] = useState({id: 0, name: 'Default', cohort: 0});
+  const [userData, updateUserData]= useReducer(reduceUserData, [{name:""}])
+
+
+
+
+  useEffect(()=>{
+
+    
+    fetch(`${process.env.REACT_APP_DATABASE}users/${userId}`)
+      .then(res=>res.json())
+      .then(res=> updateUserData({newState:res.payload})
+      //.then(res=>console.log(res.payload))
+      
+      )
+    
+
+  },[userId])
+
+
+  useEffect(()=>{
+    console.log(userData)
+  },[userData])
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -15,7 +45,7 @@ export default function App() {
           {/* Layout will have an ul inside a nav tag with Links to ToDoList and Homepage */}
           <Route path="/" element={<Layout />}>
             {/* Homepage will have h1 Hello bootcamper, h2 You're on week x, and the calander*/}
-            <Route index element={<Homepage />} />
+            <Route index element={<Homepage userData={userData} userId={userData} setUserId={setUserId} />} />
             {/* Day view will have Mood tracker component and Journal component */}
             <Route path="dayview" element={<Dayview />} />
             {/* Make a to-do list */}
--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import MyCalender from "../Calender";
 import "./style.css";
 
-function HomePage() {
+function HomePage({userData, userId, setUserId}) {
   return (
     <div className="home-page">
-      <h1> Hello, Bootcamper</h1>
-      {/* (put name here maybe?) */}
+      <h1> Hello, {userData[0].name}</h1> 
+
+      {/* Dev Tools remove before merging to main */}
+      <span>
+        <button onClick={(e)=>{
+        e.preventDefault();
+          setUserId(Number(e.target.innerText))
+        }}>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </span>
+       {/* Dev Tools remove before merging to main */}
+      {/* { (put name here maybe?) */}
       <h3>You're on week x</h3>
       <span style={{display:"flex"}}>
       <MyCalender />
