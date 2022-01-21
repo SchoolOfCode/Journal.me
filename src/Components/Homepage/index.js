20115019a6c2236a3328ae226f94523c510a37a8
Najma Elmi <nelmi199@gmail.com>
2022-01-21 01:07:31 +0000

    changed the colour and styling of a few items

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -8,9 +8,9 @@ function HomePage() {
     <div className="home-page">
       <h1> Hello, Bootcamper</h1>
       {/* (put name here maybe?) */}
-      <h2>You're on week</h2><input/>
-      <span style={{display:"flex"}}>
-      <MyCalender />
+      {/* <h2>You're on week</h2><input/> */}
+      <span style={{ display: "flex" }}>
+        <MyCalender />
       </span>
     </div>
   );
--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -4,58 +4,55 @@ import "./style.css";
 const Layout = () => {
   return (
     <>
- 
       <nav className="menu">
         <ul className="menu-items">
           <li>
-            <Link to="/" className="Home_nav_bar_font">
+            <Link to="/" className="btn">
               {" "}
-              Home 🏠{" "}
+              {/* className="Home_nav_bar_font" */} Home 🏠{" "}
             </Link>
           </li>
           <li>
-            <Link to="/dayview" className="DayView_nav_bar_font">
+            <Link to="/dayview" className="btn">
+              {" "}
+              {/* className="DayView_nav_bar_font */}
               Daily Journal 📓{" "}
             </Link>
           </li>
           <li>
-            <Link to="/todolist" className="To_Do_List_nav_bar">
+            <Link to="/todolist" className="btn">
+              {" "}
+              {/* To_Do_List_nav_bar */}
               To-Do List 📋{" "}
             </Link>
           </li>
         </ul>
       </nav>
 
-
-      <Outlet/>
+      <Outlet />
 
       <div className="navbar">
-      <span className = "link">
-      <button id= "link1"
-      href="https://vimeo.com/showcase/soc-national" >
-        Vimeo
-      </button> 
+        <span className="link">
+          <button id="link1" href="https://vimeo.com/showcase/soc-national">
+            Vimeo
+          </button>
 
-      <button id= "link2" 
-      href="https://www.notion.so/" >
-        Notion
-      </button> 
+          <button id="link2" href="https://www.notion.so/">
+            Notion
+          </button>
 
-      <button id= "link3" 
-      href="https://zoom.us/signin" >
-        Zoom
-      </button> 
-      </span>
+          <button id="link3" href="https://zoom.us/signin">
+            Zoom
+          </button>
+        </span>
 
-      <footer>
-        <p5>Created by:</p5><br/>
-          <p5>Callum Hemming</p5><br/>
-          <p5>Samira Midgley</p5><br/>
-          <p5>Najma Elmi</p5><br/>
-          <p5>Tan Coding</p5><br/>
-        <p10><strong>Copyright@NascarTanTeam</strong></p10>
-      </footer>
-
+        {/* <footer>
+          <p5>Created by:</p5>
+          <p6>Callum Hemming, Samira Midgley, Najma Elmi, Tan Coding</p6>
+          <p10>
+            <strong>Copyright@NascarTanTeam</strong>
+          </p10>
+        </footer> */}
       </div>
     </>
   );
